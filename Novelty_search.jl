a18ea0d564d952330e831180a0cf1b0d0be5c2eb
MaximeVH <maxime.vanhaeverbeke@ugent.be>
2025-04-16 11:42:29 +0200

    QD evolution

--- Novelty_search.jl
+++ Novelty_search.jl
@@ -105,13 +105,34 @@ function add_to_archive!(archive::Vector{TreeWrapper}, t_new::TreeWrapper, featu
     end
     return tree_added
 end
-## In this approach, a random forest from DecisionTree.jl is used to construct the initial population (i.e., initialize the archive).
 
-function initialize_archive(Forestmodel,features,labels, Δ_min = 30)
+## In this approach, a random forest from DecisionTree.jl is used to construct the initial population (i.e., initialize the archive).
+function init_archive(Forestmodel,features,labels, Δ_min = 30)
     forest = Forestmodel.trees
     archive = [forest[1],forest[2]]
     for tree in forest[3:end]
         add_to_archive!(archive, tree, features,labels, Δ_min)
+    end
+    return archive
+end
+
+function QD_evolution(Forestmodel, features,labels, generations::Int=100, mutation_rate::Float64=0.1, Δ_min::Int=30)
+    # Initialize the archive with the random forest (from DecisionTree.jl)
+    archive = init_archive(Forestmodel,features,labels,Δ_min)
+    println("Initial archive size: ", length(archive))
+    for gen in 1:generations #first attempt without curiosity scores, will be added later
+        # random selection of two parents from the archive
+        parent_tree1, parent_tree2 = rand(archive, 2) 
+        # convert them to mutable trees
+        parent_tree1m = to_mutable_tree(parent_tree1)  
+        parent_tree2m = to_mutable_tree(parent_tree2)
+        # apply genetic operations
+        child_tree1, child_tree2 = crossover_height_matched(parent_tree1m, parent_tree2m)
+        mutate!(child_tree1, collect(1:size(features, 2)), features, mutation_rate)
+        mutate!(child_tree2, collect(1:size(features, 2)), features, mutation_rate)
+        # convert back to immutable trees and attempt to add them to the archive
+        add_to_archive!(archive, to_decision_tree(child_tree1), features, labels, Δ_min)
+        add_to_archive!(archive, to_decision_tree(child_tree2), features, labels, Δ_min)
     end
     return archive
 end
